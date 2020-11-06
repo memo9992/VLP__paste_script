1baeb942b93728526cd33ade26fb1874235e8ff1
Maged Tesare <maged.tesare@gmail.com>
2020-11-06 07:50:05 +0100

    Py script and code.txt file

---
+++ VPL/VLP.py
@@ -0,0 +1,24 @@
+import keyboard
+import subprocess
+import time
+#it's recomended to save the java program as a txt file without the tabs ,{},() and then add them later in the VPL
+#open the file
+f = open("code.txt", "r")
+#read the file
+file = f.read()
+#path of the webbrowser
+path = 'C:\Program Files\Mozilla Firefox\\firefox.exe'
+#link of "Testat"
+tab = 'https://moodle.th-brandenburg.de/mod/vpl/forms/edit.php?id=393349&userid=22138'
+
+#open the Browser
+subprocess.Popen([path, tab])
+#wait 10 seconds
+time.sleep(10)
+# type the program
+for c in file:
+    keyboard.write(c)
+    #you can remove the sleep if you don't want to watch the program type it out
+    time.sleep(0.2)
+f.close()   
+
